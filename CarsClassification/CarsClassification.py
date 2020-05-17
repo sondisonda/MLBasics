ba23f073fd65e813de179e0e9df657113bd174a6
sondisonda <speszal97@gmail.com>
2020-05-17 02:40:48 +0200

    Implemented KNN

--- CarsClassification/CarsClassification.py
+++ CarsClassification/CarsClassification.py
@@ -6,7 +6,6 @@ import numpy as np
 from sklearn import linear_model, preprocessing
 
 data = pd.read_csv("car.data")
-print(data.head())
 
 le = preprocessing.LabelEncoder()
 buying = le.fit_transform(list(data["buying"]))
@@ -17,9 +16,23 @@ lug_boot = le.fit_transform(list(data["lug_boot"]))
 safety = le.fit_transform(list(data["safety"]))
 cls = le.fit_transform(list(data["class"]))
 
-predict = "class"  
+predict = "class"
 
 X = list(zip(buying, maint, door, persons, lug_boot, safety))
-Y = list(cls)
+Y= list(cls)
 
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1)
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size = 0.1)
+
+model = KNeighborsClassifier(n_neighbors=9)
+
+model.fit(x_train, y_train)
+acc = model.score(x_test, y_test)
+print(acc)
+
+predicted = model.predict(x_test)
+names = ["unacc", "acc", "good", "vgood"]
+
+for x in range(len(predicted)):
+    print("Predicted: ", names[predicted[x]], "Data: ", x_test[x], "Actual: ", names[y_test[x]])
+    n = model.kneighbors([x_test[x]], 9, True)
+    print("N: ", n)
